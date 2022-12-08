fcafd3a12d3cd2884e3a293fa22ff01a719df098
Trevor <tr.edewaard@gmail.com>
2022-12-08 23:59:04 +0000

    Need to figure out how to parse moves

--- d5/main.go
+++ d5/main.go
@@ -4,18 +4,17 @@ import (
     "os"
     "fmt"
     "bufio"
+    "strconv"
+    "regexp"
 )
 
 
-func moveCrates(count int, from int, to int) {
-
-}
 
 func main() {
    
     //Open file
     //readfile,err := os.Open("d5_data")
-    readfile,err := os.Open("test")
+    readfile,err := os.Open("d5_data")
     if err != nil {
         fmt.Println(err)
     }
@@ -23,7 +22,7 @@ func main() {
     filescanner := bufio.NewScanner(readfile)
 
     filescanner.Split(bufio.ScanLines)
-    var blocks []string
+    var cargo []string
     var moves []string
 
     for filescanner.Scan() {
@@ -32,9 +31,10 @@ func main() {
         data := filescanner.Text()
         if data != "" {
         if string(data[0]) != "m" {
-            blocks = append(blocks, data)
+            cargo = append(cargo, data)
         } else {
-            moves = append(moves, data)
+            re := regexp.MustCompile("[0-9]+")
+            moves = append(moves, re)
         }
     }
 
@@ -42,34 +42,88 @@ func main() {
 
     readfile.Close()
 
-    var stacks [][]string
 
-    n := len(blocks)-1
+
     //Index starts at 1 and jumps 4
     //1, 5, 9, 13
-
-    //Grabbing the number of stacks and adding the stacks to an array
-    var stack []int
-    for j := 1; j <= len(blocks[n])-1; j+=4 {
-       stack = append(stack, j) 
-    }
-
-
-    for i:=n-1; i >= 0; i-- {
-        arr := []string{}
-        for k := 1; k <= len(blocks[i])-1; k+=4 {
-            a:=string(blocks[i][k])
-            arr = append(arr, a)
+    //This will grab the number of stacks
+    /*
+    n := len(cargo)-1
+    numStacks := []int{} 
+    for i := 1; i <= len(cargo[n]); i+=4 {
+        if string(cargo[n][i]) != "" {
+            val, _ := strconv.Atoi(string(cargo[n][i]))
+            numStacks = append(numStacks, val)
         }
-       stacks = append(stacks, arr) 
+    }
+    */
+
+    n := len(cargo)-1
+
+    //Make map
+    stacks := make(map[int][]string)
+    for i:=n-1; i>=0; i-- {
+        for j:=1; j <= len(cargo[i]); j+=4 {
+            val, _ := strconv.Atoi(string(cargo[n][j]))
+            if string(cargo[i][j]) != " " {
+                stacks[val] = append(stacks[val], string(cargo[i][j])) 
+            }
+        }
+    }
+///*
+    moveCrates := func(count int, from int, to int) {
+        for i := 1; i<=count; i++ {
+            f := stacks[from]
+            if len(f) == 0 {
+                break
+            }
+            stacks[to] = append(stacks[to], f[len(f)-1])
+            stacks[from] = f[:len(f)-1]
+        }
+
+
+
+        /*
+        f := stacks[from]
+        items := []string{}
+        if count >= len(stacks[from]) {
+            items = stacks[from]
+            stacks[from] = []string{} 
+        } else{
+            items = f[len(f)-count:]
+            stacks[from] = f[0:len(f)-count]
+        }
+        stacks[to] = append(stacks[to], items...) 
+        */
     }
 
-    for _,i := range stacks {
-        fmt.Println(i)
+    //Iterate through moves
+    for i := 0; i <= len(moves)-1; i++ {
+
+        for j := 0; j <=len(moves[i]); j++ {
+
+        }
+
+
+
+        //For each line of moves grab the character and 
+        c, _ := strconv.Atoi(string(moves[i][5]))
+        f, _ := strconv.Atoi(string(moves[i][12]))
+        t, _ := strconv.Atoi(string(moves[i][17]))
+        moveCrates(c, f, t)
+    }
+//*/
+
+
+    //Print out the current stacks
+    for i:=1; i<=100; i++ {
+        if val, ok := stacks[i]; ok {
+            fmt.Println(i, val)
+        }
     }
 
     /*
-    for _,i := range blocks {
+    for _,i := range cargo {
         fmt.Println(i)
     }
     for _,i := range moves {
