3d753478aa75773274a068d4a62e40db0ef0745f
Trevor <tr.edewaard@gmail.com>
2023-01-12 17:51:32 +0000

    Rust d6 part 2 done

--- rust/day6/src/main.rs
+++ rust/day6/src/main.rs
@@ -6,31 +6,9 @@ fn main() {
 
     let character_bytes = input.into_bytes();
 
-
-    //let mut bytes_iter = character_bytes.windows(4);
-
-    /*
-    for byte in bytes_iter {
-        let mut check = 0;
-        'outer: for i in 0..4 {
-             for j in 0..4 {
-                if i != j && byte[i] == byte[j] {
-                    check = 1;
-                    break 'outer 
-                }
-            }
-        }
-        if check == 0 {
-            let next = bytes_iter.next().unwrap();
-            let first = next[0];
-        }
-    }
-    */
-
-
     let mut next_check = 0;
-    let mut count = 3;
-    for byte in character_bytes.windows(4) {
+    let mut count = 13;
+    for byte in character_bytes.windows(14) {
         count += 1;
         println!("{}", count);
         println!("{:?}", byte);
@@ -38,8 +16,8 @@ fn main() {
             break;    
         }
         let mut check = 0;
-        'outer: for i in 0..4 {
-             for j in 0..4 {
+        'outer: for i in 0..14 {
+             for j in 0..14 {
                 if i != j && byte[i] == byte[j] {
                     check = 1;
                     println!("{} is equal to {}", byte[i], byte[j]);
@@ -51,7 +29,6 @@ fn main() {
             next_check = 1;
         }
     }
-
 
     println!("{}", count-1)
 }
